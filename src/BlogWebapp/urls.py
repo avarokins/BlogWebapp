7b1324f1ec81919655673f19f3be75950d9bf437
Avarokin Saini <saini7@purdue.edu>
2020-05-25 20:38:50 +0530

    Basic autodidactic education.

--- src/BlogWebapp/urls.py
+++ src/BlogWebapp/urls.py
@@ -18,11 +18,13 @@ from django.urls import path, re_path
 from .views import (
 	home_page,
  	about_page,
- 	contact_page)
+ 	contact_page,
+    example_page)
 
 urlpatterns = [
+    path('', home_page),
     path('admin/', admin.site.urls),
     path('about/', about_page),
     path('contact/', contact_page),
-    path('', home_page),
+    path('example/', example_page),
 ]
--- src/BlogWebapp/views.py
+++ src/BlogWebapp/views.py
@@ -1,13 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 def home_page(request):
-	return render(request,'hello_world.html')
+	context = {'title':'Home Page',
+				'my_list':[1,2,3,4,5]}
+	return render( request,'home.html', context )
 
 
 def about_page(request):
-	return HttpResponse("<h1>About Us</h1>")
+	return render( request,'about.html', {'title':'About us'} )
 
 
 def contact_page(request):
-	return HttpResponse("<h1>Contact Us</h1>")
+	return render( request,'hello_world.html', {'title':'Contact us'} )
+
+
+def example_page(request):
+	context = {'title':'Example title'}
+	template_name = 'base.html'
+	template_object = get_template(template_name)
+	return HttpResponse(template_object.render(context))
